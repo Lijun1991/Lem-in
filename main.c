ca718e6f8863e2cf50a4a3bf976397c59663a4bb
Lijun Wang <wang283808065@gmail.com>
2017-06-27 14:38:25 -0700

    move ants new algrithm

--- main.c
+++ main.c
@@ -98,52 +98,109 @@ void	update_path_taken(int *path_taken, int room_nbr, int *path)
 	}
 }
 
-void	move_ants(t_leminfo *info, int *path, int len, int ants_total)
+// void	move_ants(t_leminfo *info, int *path, int len, int ants_total)
+// {
+// 	int		*path_taken;
+// 	t_ant	*ants;
+// 	int		indx_ant;
+// 	int		indx_room;
+// 	int 	tmp_len;
+// 	int		count;
+// 	int 	tmp_index_room;
+
+// 	tmp_len = len;
+// 	count = 0;
+// 	// indx_room = 1;
+// 	path_taken = get_path_taken(info);
+
+// 	ants = initial_ants(ants_total, path);
+
+// 	ft_printf("hello, ants_total is %d, path[0] is %d, info->nbr_of_ant %s, len is %d, room_total is %d\n", ants_total, path[0], info->nbr_of_ant, len, info->room_total);
+// 	while (count < tmp_len - 1 + ants_total - 1)//ants[ants_total - 1].cur_room != path[len - 1] &&
+// 	{
+// 		ft_printf("hello1\n");
+// 		indx_ant = 0;
+// 		indx_room = 1;
+// 		tmp_index_room = info->room_total;
+// 		while (tmp_index_room > 1)
+// 		{
+// 			while (indx_ant < ants_total)
+// 			{
+// 				ft_printf("hello inside\n");
+// 				if (ants[ants_total - 1].cur_room != path[0])
+// 					tmp_len--;
+// 				// ft_printf("path_taken[ants[indx_ant].next_room] is %d\n", path_taken[ants[indx_ant].next_room]);
+// 				// ft_printf("ants[indx_ant].next_room is %d\n", ants[indx_ant].next_room);
+// 				// ft_printf("ants[indx_ant] is %d\n", ants[indx_ant]);
+// 				if (!path_taken[ants[indx_ant].next_room] && ants[indx_ant].cur_room != path[len - 1])
+// 				{
+// 					get_ants_status(ants, path, indx_ant, indx_room);
+// 					update_path_taken(path_taken, ants[indx_ant].cur_room, path);
+// 					ft_printf("L%d-%d ", indx_ant + 1, path[indx_room]);
+// 					indx_room++;
+// 				}
+// 				indx_ant++;
+// 			}
+// 			tmp_index_room--;
+// 		}
+// 		ft_printf("\n");
+// 		count++;
+// 	}
+
+// }
+
+void	move_ants(t_leminfo *info, int *path, int path_len, int ants_total)
 {
 	int		*path_taken;
-	t_ant	*ants;
-	int		indx_ant;
-	int		indx_room;
-	int 	tmp_len;
-	int		count;
+	int		tmp_path_len;
+	int		ant_nbr;
+	int		ant_get_end;
+	int		move_times;
 
-	tmp_len = len;
-	count = 0;
-	// indx_room = 1;
 	path_taken = get_path_taken(info);
+	move_times = path_len - 1 + ants_total - 1;
 
-	ants = initial_ants(ants_total, path);
-
-	ft_printf("hello, ants_total is %d, path[0] is %d, info->nbr_of_ant %s, len is %d, room_total is %d\n", ants_total, path[0], info->nbr_of_ant, len, info->room_total);
-	while (count < tmp_len - 1 + ants_total - 1)//ants[ants_total - 1].cur_room != path[len - 1] &&
+	path_taken[path[0]] = 1;
+	while (move_times)
 	{
-		ft_printf("hello1\n");
-		indx_ant = 0;
-		indx_room = 1;
-		while (indx_ant < ants_total)
+		ant_nbr = 1;
+		ant_get_end = 0;
+		tmp_path_len = path_len - 1;
+		while (tmp_path_len > 0)
 		{
-			ft_printf("hello inside\n");
-			if (ants[ants_total - 1].cur_room != path[0])
-				tmp_len--;
-			// ft_printf("path_taken[ants[indx_ant].next_room] is %d\n", path_taken[ants[indx_ant].next_room]);
-			// ft_printf("ants[indx_ant].next_room is %d\n", ants[indx_ant].next_room);
-			// ft_printf("ants[indx_ant] is %d\n", ants[indx_ant]);
-			while (indx_room < info->room_total)
+			ant_nbr = ant_nbr + ant_get_end;
+			ft_printf("tmp_path_len is %d, path_taken[path[tmp_path_len - 1]] is %d, path_taken[path[tmp_path_len] is %d\n", tmp_path_len, path_taken[path[tmp_path_len - 1]], path_taken[path[tmp_path_len]]);
+			if (path_taken[path[tmp_path_len]] && tmp_path_len == path_len)
 			{
-				if (!path_taken[ants[indx_ant].next_room] && ants[indx_ant].cur_room != path[len - 1])
-				{
-					get_ants_status(ants, path, indx_ant, indx_room);
-					update_path_taken(path_taken, ants[indx_ant].cur_room, path);
-					ft_printf("L%d-%d ", indx_ant + 1, path[indx_room]);
-					indx_room++;
-				}
+			// 	ft_printf("L%d-%d ", ant_nbr, path[tmp_path_len]);
+			// 	path_taken[path[tmp_path_len - 1]] = 0;
+				ant_get_end++;
 			}
-			indx_ant++;
+			if (path_taken[path[tmp_path_len - 1]] && !(path_taken[path[tmp_path_len]]))
+			{
+				ft_printf("find it\n");
+				ft_printf("L%d-%d ", ant_nbr, path[tmp_path_len]);
+				
+				if (path[tmp_path_len] == path)
+				if (tmp_path_len != path_len - 1)
+					path_taken[path[tmp_path_len]] = 1;
+				if (tmp_path_len != 1)
+					path_taken[path[tmp_path_len - 1]] = 0;
+				ant_nbr++;
+			}
+			tmp_path_len--;
+			// ant_nbr++;
 		}
 		ft_printf("\n");
-		count++;
+		move_times--;
 	}
 
+	// if (path)
+	// 	;
+	// if (ants_total)
+	// 	;
+	// if (len)
+	// 	;
 }
 
 int	print_lemin_result(t_leminfo *info, char **map)
